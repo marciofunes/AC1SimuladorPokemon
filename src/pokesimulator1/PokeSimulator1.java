5f56cc8e11c6f03b9fe9d10e94503c348ec3780b
Marcio Funes <marciofunes@gmail.com>
2021-10-06 22:31:35 -0300

    adicionei uma nova linha

--- src/pokesimulator1/PokeSimulator1.java
+++ src/pokesimulator1/PokeSimulator1.java
@@ -5,6 +5,7 @@ public class PokeSimulator1 {
     public static void main(String[] args) {
         
         //Instanciando um objeto
+        //testando git
         Pokemon meuPokemon = new Pokemon("Charmander", "Fogo", 100);
         Pokemon outroPokemon = new Pokemon("Staryu", "Agua", 50);
         
